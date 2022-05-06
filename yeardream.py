f1a02e8ff81e97b6a1ac9c62ae0e6311fd2ada7a
municef1 <esemoon@gmail.com>
2022-05-06 16:25:05 +0900

    메모메모

---
+++ yeardream.py
@@ -0,0 +1,9 @@
+for i in range(1, 20+1):
+        if i % 15 ==0:
+                print('15배수')
+        elif i %3 == 0:
+                print('3배수')
+        elif i % 5 ==0:
+                print('5배수')
+        else:
+                print(i)
